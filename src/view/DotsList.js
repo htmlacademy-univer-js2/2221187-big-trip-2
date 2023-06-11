7f717ad1032c64d4b8f51975c5527aa55a9f39ce
kav0ish0 <Vladimir.Ozornin@urfu.me>
2023-06-11 16:44:29 +0500

    Style fixing 3

--- src/view/DotsList.js
+++ src/view/DotsList.js
@@ -1,12 +1,12 @@
 import createElement from '../createElement';
 
-const dotsTemplate = () => `<ul class="trip-events__list"></ul>`;
+const dotsTemplate = () => '<ul class="trip-events__list"></ul>';
 
 class DotsView {
   getTemplate() {
     return dotsTemplate();
   }
-  
+
   get element() {
     if (!this._element) {
       this._element = createElement(this.getTemplate());
